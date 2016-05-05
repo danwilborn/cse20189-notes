76ba664c30c5faf55b22ab9640709f7b9f2f76c4
Daniel Wilborn <dwilborn@nd.edu>
2016-05-05 09:15:11 -0400

    adkfjapsdkjf

--- notes/final/portScanner.py
+++ notes/final/portScanner.py
@@ -11,18 +11,18 @@ ADDRESS = 'student00.cse.nd.edu'
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Allocate TCP socket
 for PORT in range(9300, 9399):
+	print PORT
 	try:
 		client.connect((ADDRESS, PORT))                             # Connect to server with ADDRESS and PORT
 		stream = client.makefile('w+')                              # Create file object from socket
-		data   = sys.stdin.readline()                               # Read from STDIN
+		data   = stream.readline()                               # Read from STDIN
 		while data:
 			stream.flush()
 
 			# Read from Server to STDOUT
 			data = stream.readline()
+			sys.stdout.write(data)
 
-			# Read from STDIN
-			data = sys.stdin.readline()
 	except socket.error:
 		pass
 
