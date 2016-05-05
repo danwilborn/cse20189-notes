a4b1e0178fccd6e550a30fbfeebb2345f06d3a29
Daniel Wilborn <dwilborn@nd.edu>
2016-05-04 22:51:56 -0400

    ppaodsijf

--- notes/final/portScanner.py
+++ notes/final/portScanner.py
@@ -5,35 +5,26 @@ import sys
 
 # Constants
 
-ADDRESS = '127.0.0.1'
-PROGRAM = os.path.basename(sys.argv[0])
+ADDRESS = 'student00.cse.nd.edu'
 
 # Echo Client
 
-
-for PORT in (9300, 9399):
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Allocate TCP socket
+for PORT in range(9300, 9399):
 	try:
-		client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Allocate TCP socket
 		client.connect((ADDRESS, PORT))                             # Connect to server with ADDRESS and PORT
+		stream = client.makefile('w+')                              # Create file object from socket
+		data   = sys.stdin.readline()                               # Read from STDIN
+		while data:
+			stream.flush()
+
+			# Read from Server to STDOUT
+			data = stream.readline()
+
+			# Read from STDIN
+			data = sys.stdin.readline()
 	except socket.error:
 		pass
 
 
-stream = client.makefile('w+')                              # Create file object from socket
 
-
-
-while data:
-    
-	data   = sys.stdin.readline()     # Read from STDIN
-
-	# Send STDIN to Server
-	stream.write(data)
-	stream.flush()
-
-	# Read from Server to STDOUT
-	data = stream.readline()
-	sys.stdout.write(data)
-
-	# Read from STDIN
-	data = sys.stdin.readline()
